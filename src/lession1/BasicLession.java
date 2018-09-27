f69e66fe7a1a44260bd4179230d86220d334f3d9
khaliddhaliLS <khalid.linkstaff@gmail.com>
2018-09-27 12:13:43 +0900

    First Commit: As local master. Project is not empty though.

---
+++ src/lession1/BasicLession.java
@@ -0,0 +1,69 @@
+package lession1;
+/*
+ * Cover: 1. Data Type
+ * 	Data Type: Primitive Data
+ * 		Byte    -> 8-bit signed two's complement integer.
+ * 		short   -> 16-bit signed two's complement integer.
+ * 		int     -> 32-bit signed two's complement integer.
+ * 		long    -> 64-bit two's complement integer. 
+ * 		float   -> 32-bit single-precision, IEEE 754 floating point.
+ * 		double  -> 64-bit double-precision, IEEE 754 floating point.
+ * 		boolean -> true/false
+ * 		char    -> 16-bit Unicode character.
+ *  Fix: why any string input is omitted after taking any of int, float, double, etc type data
+ * 
+ */
+
+import java.util.Scanner;
+
+
+public class BasicLession {
+	
+
+	public BasicLession() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// To take any input from user create a scanner object.
+		var scn = new Scanner(System.in);
+		
+		String usrName;
+		int intNum = 235;
+		String gender;
+		float age = (float) 53.35; //default floating value in java is double, hence casting.
+		double doubleNum = 5234234523.21345;
+		
+		System.out.println("Hello World\n");
+		System.out.format("int num: %d , float num: %f, double num: %f%n", intNum, age, doubleNum);
+		
+		System.out.println("What's your name?");
+		usrName = scn.nextLine();
+		System.out.println("Hello "+usrName+"!");
+		
+		//if(scn.hasNextLine())
+			System.out.println("What's your age?");
+		/* we need to use following float wrapper, because of .nextFloat() or .nextInt() 
+		 * or .nextDouble() doesn't read the nextline you enter after you enter a number.
+		 * hence it won't escape any string input to take as a input.
+		 */
+		age = Float.parseFloat(scn.nextLine());
+		
+		System.out.println("\nGender?");
+		gender = scn.nextLine().toLowerCase();
+		switch (gender) {
+		case "male" :
+			System.out.println("You're a male, great!");
+			break;
+		case "female": 
+			System.out.println("Wow! you're a female!");
+
+		default:
+			System.out.println("Oww! you're not sure about your sex :(");
+			break;
+		}
+		
+		scn.close();
+	}
+
+}
---
+++ src/lession2/NonPrimitiveDataType.java
@@ -0,0 +1,62 @@
+package lession2;
+
+
+/*
+ * Cover: 
+ *  1. Array
+ *  2. string
+ *  3. 
+ *  
+ *   Array:
+ *    Way of Declaration:
+ *    int[] array1 = {1, 34, 4, 12}
+ *    
+ *    int[] array2 = new int[4];
+ *     array2 = {1, 32, 53, 54};
+ *     
+ *    int[]array3 = new int[]{3, 4, 2, 4, 7};
+ *
+ *    
+ */
+
+public class NonPrimitiveDataType {
+
+	public NonPrimitiveDataType() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Array~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+		int[] intArray = {0, 1, 1, 2, 3, 5};
+		float[] floatArray = new float[] { 3.5f, 35.54f, 3.54f};
+		
+		//How to retrive/ print data from array
+		
+		//conventional way: using loop (for/while like follow)
+		for(int i = 0; i <  intArray.length; i++) {
+			System.out.print(intArray[i]+ " ");
+		}
+		
+		//Convenient way: This is important
+		System.out.print("\nConvinient Way: ");
+		for(float i : floatArray) { //this i will hold the data of each element of array.
+			System.out.print(i + ", ");			
+		}
+		
+		/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~String~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+		//In java String is a built in class
+		String aString = "This is a string";
+		System.out.println(aString);
+		
+		System.out.println(aString.replace('a', 'm'));
+		System.out.println(aString.indexOf('s'));
+		
+		
+		
+		
+		
+		
+	}
+}
